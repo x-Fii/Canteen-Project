71807b205ecc85f9879b590f2952ded0a721a463
x-Fii <kl2311015179@student.uptm.edu.my>
2026-03-10 14:26:27 +0800

    fix: Update Vite configuration to set build target to Chrome 87

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  
   server: {
     host: "::",
     port: 8080,
@@ -99,6 +100,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
+    target : "chrome87",
     rollupOptions: {
       output: {
         manualChunks: {
